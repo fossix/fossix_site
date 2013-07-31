ee1f6bc580cf0554c784a6f810bcdb7260e36a45
Santosh Sivaraj <santosh@fossix.org>
2013-07-31 17:27:03 +0530

    Added a 404 error page and handler
    Signed-off-by: Santosh Sivaraj <santosh@fossix.org>

--- fossix/application.py
+++ fossix/application.py
@@ -1,4 +1,4 @@
-from flask import Flask, g
+from flask import Flask, g, render_template
 from fossix.extensions import oid, cache, lm
 from fossix.models import fdb
 from fossix import views
@@ -81,5 +81,8 @@ def configure_extensions(app):
 	     safe_mode='remove',
 	     output_format="html5")
 
+# This function also has the error pages
 def configure_errorhandlers(app):
-    pass
+    @app.errorhandler(404)
+    def not_found(e):
+	return render_template('error/404.html', err=e), 404
